f5ab1c028d78cc82a600a74b7e57dac0a62dab39
root <8974108+qwenode@users.noreply.github.com>
2024-08-05 14:37:12 +0800

    11

--- strings.go
+++ strings.go
@@ -8,6 +8,7 @@ import (
     "regexp"
     "strconv"
     "strings"
+    "sync"
 )
 
 var (
@@ -338,4 +339,155 @@ func (r S) Crc32() string {
         return ""
     }
     return B(r).Crc32()
+}
+
+// ToSnake converts a string to snake_case
+func (r S) ToSnake() string {
+    return r.ToDelimited('_')
+}
+
+func (r S) ToSnakeWithIgnore(ignore string) string {
+    return r.ToScreamingDelimited('_', ignore, false)
+}
+
+// ToScreamingSnake converts a string to SCREAMING_SNAKE_CASE
+func (r S) ToScreamingSnake() string {
+    return r.ToScreamingDelimited('_', "", true)
+}
+
+// ToKebab converts a string to kebab-case
+func (r S) ToKebab() string {
+    return r.ToDelimited('-')
+}
+
+// ToScreamingKebab converts a string to SCREAMING-KEBAB-CASE
+func (r S) ToScreamingKebab() string {
+    return r.ToScreamingDelimited('-', "", true)
+}
+
+// ToDelimited converts a string to delimited.snake.case
+// (in this case `delimiter = '.'`)
+func (r S) ToDelimited(delimiter uint8) string {
+    return r.ToScreamingDelimited(delimiter, "", false)
+}
+
+// ToScreamingDelimited converts a string to SCREAMING.DELIMITED.SNAKE.CASE
+// (in this case `delimiter = '.'; screaming = true`)
+// or delimited.snake.case
+// (in this case `delimiter = '.'; screaming = false`)
+func (r S) ToScreamingDelimited(delimiter uint8, ignore string, screaming bool) string {
+    s := r.TrimSpace().String()
+    n := strings.Builder{}
+    n.Grow(len(s) + 2) // nominal 2 bytes of extra space for inserted delimiters
+    for i, v := range []byte(s) {
+        vIsCap := v >= 'A' && v <= 'Z'
+        vIsLow := v >= 'a' && v <= 'z'
+        if vIsLow && screaming {
+            v += 'A'
+            v -= 'a'
+        } else if vIsCap && !screaming {
+            v += 'a'
+            v -= 'A'
+        }
+        
+        // treat acronyms as words, eg for JSONData -> JSON is a whole word
+        if i+1 < len(s) {
+            next := s[i+1]
+            vIsNum := v >= '0' && v <= '9'
+            nextIsCap := next >= 'A' && next <= 'Z'
+            nextIsLow := next >= 'a' && next <= 'z'
+            nextIsNum := next >= '0' && next <= '9'
+            // add underscore if next letter case type is changed
+            if (vIsCap && (nextIsLow || nextIsNum)) || (vIsLow && (nextIsCap || nextIsNum)) || (vIsNum && (nextIsCap || nextIsLow)) {
+                prevIgnore := ignore != "" && i > 0 && strings.ContainsAny(string(s[i-1]), ignore)
+                if !prevIgnore {
+                    if vIsCap && nextIsLow {
+                        if prevIsCap := i > 0 && s[i-1] >= 'A' && s[i-1] <= 'Z'; prevIsCap {
+                            n.WriteByte(delimiter)
+                        }
+                    }
+                    n.WriteByte(v)
+                    if vIsLow || vIsNum || nextIsNum {
+                        n.WriteByte(delimiter)
+                    }
+                    continue
+                }
+            }
+        }
+        
+        if (v == ' ' || v == '_' || v == '-' || v == '.') && !strings.ContainsAny(string(v), ignore) {
+            // replace space/underscore/hyphen/dot with delimiter
+            n.WriteByte(delimiter)
+        } else {
+            n.WriteByte(v)
+        }
+    }
+    
+    return n.String()
+}
+
+var uppercaseAcronym = sync.Map{}
+
+// "ID": "id",
+
+// ConfigureAcronym allows you to add additional words which will be considered acronyms
+func ConfigureAcronym(key, val string) {
+    uppercaseAcronym.Store(key, val)
+}
+
+// Converts a string to CamelCase
+func (r S) toCamelInitCase(initCase bool) string {
+    s := r.TrimSpace().String()
+    if s == "" {
+        return s
+    }
+    a, hasAcronym := uppercaseAcronym.Load(s)
+    if hasAcronym {
+        s = a.(string)
+    }
+    
+    n := strings.Builder{}
+    n.Grow(len(s))
+    capNext := initCase
+    prevIsCap := false
+    for i, v := range []byte(s) {
+        vIsCap := v >= 'A' && v <= 'Z'
+        vIsLow := v >= 'a' && v <= 'z'
+        if capNext {
+            if vIsLow {
+                v += 'A'
+                v -= 'a'
+            }
+        } else if i == 0 {
+            if vIsCap {
+                v += 'a'
+                v -= 'A'
+            }
+        } else if prevIsCap && vIsCap && !hasAcronym {
+            v += 'a'
+            v -= 'A'
+        }
+        prevIsCap = vIsCap
+        
+        if vIsCap || vIsLow {
+            n.WriteByte(v)
+            capNext = false
+        } else if vIsNum := v >= '0' && v <= '9'; vIsNum {
+            n.WriteByte(v)
+            capNext = true
+        } else {
+            capNext = v == '_' || v == ' ' || v == '-' || v == '.'
+        }
+    }
+    return n.String()
+}
+
+// ToCamel converts a string to CamelCase
+func (r S) ToCamel() string {
+    return r.toCamelInitCase(true)
+}
+
+// ToLowerCamel converts a string to lowerCamelCase
+func (r S) ToLowerCamel() string {
+    return r.toCamelInitCase(false)
 }
--- strings_test.go
+++ strings_test.go
@@ -1301,3 +1301,388 @@ func TestRString_StripHtml(t *testing.T) {
         )
     }
 }
+
+func toSnake(tb testing.TB) {
+    cases := [][]string{
+        {"testCase", "test_case"},
+        {"TestCase", "test_case"},
+        {"Test Case", "test_case"},
+        {" Test Case", "test_case"},
+        {"Test Case ", "test_case"},
+        {" Test Case ", "test_case"},
+        {"test", "test"},
+        {"test_case", "test_case"},
+        {"Test", "test"},
+        {"", ""},
+        {"ManyManyWords", "many_many_words"},
+        {"manyManyWords", "many_many_words"},
+        {"AnyKind of_string", "any_kind_of_string"},
+        {"numbers2and55with000", "numbers_2_and_55_with_000"},
+        {"JSONData", "json_data"},
+        {"userID", "user_id"},
+        {"AAAbbb", "aa_abbb"},
+        {"1A2", "1_a_2"},
+        {"A1B", "a_1_b"},
+        {"A1A2A3", "a_1_a_2_a_3"},
+        {"A1 A2 A3", "a_1_a_2_a_3"},
+        {"AB1AB2AB3", "ab_1_ab_2_ab_3"},
+        {"AB1 AB2 AB3", "ab_1_ab_2_ab_3"},
+        {"some string", "some_string"},
+        {" some string", "some_string"},
+    }
+    for _, i := range cases {
+        in := i[0]
+        out := i[1]
+        result := S(in).ToSnake()
+        if result != out {
+            tb.Errorf("%q (%q != %q)", in, result, out)
+        }
+    }
+}
+
+func TestToSnake(t *testing.T) { toSnake(t) }
+
+func BenchmarkToSnake(b *testing.B) {
+    benchmarkSnakeTest(b, toSnake)
+}
+
+func toSnakeWithIgnore(tb testing.TB) {
+    cases := [][]string{
+        {"testCase", "test_case"},
+        {"TestCase", "test_case"},
+        {"Test Case", "test_case"},
+        {" Test Case", "test_case"},
+        {"Test Case ", "test_case"},
+        {" Test Case ", "test_case"},
+        {"test", "test"},
+        {"test_case", "test_case"},
+        {"Test", "test"},
+        {"", ""},
+        {"ManyManyWords", "many_many_words"},
+        {"manyManyWords", "many_many_words"},
+        {"AnyKind of_string", "any_kind_of_string"},
+        {"numbers2and55with000", "numbers_2_and_55_with_000"},
+        {"JSONData", "json_data"},
+        {"AwesomeActivity.UserID", "awesome_activity.user_id", "."},
+        {"AwesomeActivity.User.Id", "awesome_activity.user.id", "."},
+        {"AwesomeUsername@Awesome.Com", "awesome_username@awesome.com", ".@"},
+        {"lets-ignore all.of dots-and-dashes", "lets-ignore_all.of_dots-and-dashes", ".-"},
+    }
+    for _, i := range cases {
+        in := i[0]
+        out := i[1]
+        var ignore string
+        ignore = ""
+        if len(i) == 3 {
+            ignore = i[2]
+        }
+        result := S(in).ToSnakeWithIgnore(ignore)
+        if result != out {
+            istr := ""
+            if len(i) == 3 {
+                istr = " ignoring '" + i[2] + "'"
+            }
+            tb.Errorf("%q (%q != %q%s)", in, result, out, istr)
+        }
+    }
+}
+
+func TestToSnakeWithIgnore(t *testing.T) { toSnakeWithIgnore(t) }
+
+func BenchmarkToSnakeWithIgnore(b *testing.B) {
+    benchmarkSnakeTest(b, toSnakeWithIgnore)
+}
+
+func toDelimited(tb testing.TB) {
+    cases := [][]string{
+        {"testCase", "test@case"},
+        {"TestCase", "test@case"},
+        {"Test Case", "test@case"},
+        {" Test Case", "test@case"},
+        {"Test Case ", "test@case"},
+        {" Test Case ", "test@case"},
+        {"test", "test"},
+        {"test_case", "test@case"},
+        {"Test", "test"},
+        {"", ""},
+        {"ManyManyWords", "many@many@words"},
+        {"manyManyWords", "many@many@words"},
+        {"AnyKind of_string", "any@kind@of@string"},
+        {"numbers2and55with000", "numbers@2@and@55@with@000"},
+        {"JSONData", "json@data"},
+        {"userID", "user@id"},
+        {"AAAbbb", "aa@abbb"},
+        {"test-case", "test@case"},
+    }
+    for _, i := range cases {
+        in := i[0]
+        out := i[1]
+        result := S(in).ToDelimited('@')
+        if result != out {
+            tb.Errorf("%q (%q != %q)", in, result, out)
+        }
+    }
+}
+
+func TestToDelimited(t *testing.T) { toDelimited(t) }
+
+func BenchmarkToDelimited(b *testing.B) {
+    benchmarkSnakeTest(b, toDelimited)
+}
+
+func toScreamingSnake(tb testing.TB) {
+    cases := [][]string{
+        {"testCase", "TEST_CASE"},
+    }
+    for _, i := range cases {
+        in := i[0]
+        out := i[1]
+        result := S(in).ToScreamingSnake()
+        if result != out {
+            tb.Errorf("%q (%q != %q)", in, result, out)
+        }
+    }
+}
+
+func TestToScreamingSnake(t *testing.T) { toScreamingSnake(t) }
+
+func BenchmarkToScreamingSnake(b *testing.B) {
+    benchmarkSnakeTest(b, toScreamingSnake)
+}
+
+func toKebab(tb testing.TB) {
+    cases := [][]string{
+        {"testCase", "test-case"},
+    }
+    for _, i := range cases {
+        in := i[0]
+        out := i[1]
+        result := S(in).ToKebab()
+        if result != out {
+            tb.Errorf("%q (%q != %q)", in, result, out)
+        }
+    }
+}
+
+func TestToKebab(t *testing.T) { toKebab(t) }
+
+func BenchmarkToKebab(b *testing.B) {
+    benchmarkSnakeTest(b, toKebab)
+}
+
+func toScreamingKebab(tb testing.TB) {
+    cases := [][]string{
+        {"testCase", "TEST-CASE"},
+    }
+    for _, i := range cases {
+        in := i[0]
+        out := i[1]
+        result := S(in).ToScreamingKebab()
+        if result != out {
+            tb.Errorf("%q (%q != %q)", in, result, out)
+        }
+    }
+}
+
+func TestToScreamingKebab(t *testing.T) { toScreamingKebab(t) }
+
+func BenchmarkToScreamingKebab(b *testing.B) {
+    benchmarkSnakeTest(b, toScreamingKebab)
+}
+
+func toScreamingDelimited(tb testing.TB) {
+    cases := [][]string{
+        {"testCase", "TEST.CASE"},
+    }
+    for _, i := range cases {
+        in := i[0]
+        out := i[1]
+        result := S(in).ToScreamingDelimited('.', "", true)
+        if result != out {
+            tb.Errorf("%q (%q != %q)", in, result, out)
+        }
+    }
+}
+
+func TestToScreamingDelimited(t *testing.T) { toScreamingDelimited(t) }
+
+func BenchmarkToScreamingDelimited(b *testing.B) {
+    benchmarkSnakeTest(b, toScreamingDelimited)
+}
+
+func toScreamingDelimitedWithIgnore(tb testing.TB) {
+    cases := [][]string{
+        {"AnyKind of_string", "ANY.KIND OF.STRING", ".", " "},
+    }
+    for _, i := range cases {
+        in := i[0]
+        out := i[1]
+        delimiter := i[2][0]
+        ignore := i[3][0]
+        result := S(in).ToScreamingDelimited(delimiter, string(ignore), true)
+        if result != out {
+            istr := ""
+            if len(i) == 4 {
+                istr = " ignoring '" + i[3] + "'"
+            }
+            tb.Errorf("%q (%q != %q%s)", in, result, out, istr)
+        }
+    }
+}
+
+func TestToScreamingDelimitedWithIgnore(t *testing.T) { toScreamingDelimitedWithIgnore(t) }
+
+func BenchmarkToScreamingDelimitedWithIgnore(b *testing.B) {
+    benchmarkSnakeTest(b, toScreamingDelimitedWithIgnore)
+}
+
+func benchmarkSnakeTest(b *testing.B, fn func(testing.TB)) {
+    for n := 0; n < b.N; n++ {
+        fn(b)
+    }
+}
+
+func toCamel(tb testing.TB) {
+    cases := [][]string{
+        {"test_case", "TestCase"},
+        {"test.case", "TestCase"},
+        {"test", "Test"},
+        {"TestCase", "TestCase"},
+        {" test  case ", "TestCase"},
+        {"", ""},
+        {"many_many_words", "ManyManyWords"},
+        {"AnyKind of_string", "AnyKindOfString"},
+        {"odd-fix", "OddFix"},
+        {"numbers2And55with000", "Numbers2And55With000"},
+        {"ID", "Id"},
+        {"CONSTANT_CASE", "ConstantCase"},
+    }
+    for _, i := range cases {
+        in := i[0]
+        out := i[1]
+        result := S(in).ToCamel()
+        if result != out {
+            tb.Errorf("%q (%q != %q)", in, result, out)
+        }
+    }
+}
+
+func TestToCamel(t *testing.T) {
+    toCamel(t)
+}
+
+func BenchmarkToCamel(b *testing.B) {
+    benchmarkCamelTest(b, toCamel)
+}
+
+func toLowerCamel(tb testing.TB) {
+    cases := [][]string{
+        {"foo-bar", "fooBar"},
+        {"TestCase", "testCase"},
+        {"", ""},
+        {"AnyKind of_string", "anyKindOfString"},
+        {"AnyKind.of-string", "anyKindOfString"},
+        {"ID", "id"},
+        {"some string", "someString"},
+        {" some string", "someString"},
+        {"CONSTANT_CASE", "constantCase"},
+    }
+    for _, i := range cases {
+        in := i[0]
+        out := i[1]
+        result := S(in).ToLowerCamel()
+        if result != out {
+            tb.Errorf("%q (%q != %q)", in, result, out)
+        }
+    }
+}
+
+func TestToLowerCamel(t *testing.T) {
+    toLowerCamel(t)
+}
+
+func TestCustomAcronymsToCamel(t *testing.T) {
+    tests := []struct {
+        name         string
+        acronymKey   string
+        acronymValue string
+        expected     string
+    }{
+        {
+            name:         "API Custom Acronym",
+            acronymKey:   "API",
+            acronymValue: "api",
+            expected:     "Api",
+        },
+        {
+            name:         "ABCDACME Custom Acroynm",
+            acronymKey:   "ABCDACME",
+            acronymValue: "AbcdAcme",
+            expected:     "AbcdAcme",
+        },
+        {
+            name:         "PostgreSQL Custom Acronym",
+            acronymKey:   "PostgreSQL",
+            acronymValue: "PostgreSQL",
+            expected:     "PostgreSQL",
+        },
+    }
+    for _, test := range tests {
+        t.Run(
+            test.name, func(t *testing.T) {
+                ConfigureAcronym(test.acronymKey, test.acronymValue)
+                if result := S(test.acronymKey).ToCamel(); result != test.expected {
+                    t.Errorf("expected custom acronym result %s, got %s", test.expected, result)
+                }
+            },
+        )
+    }
+}
+
+func TestCustomAcronymsToLowerCamel(t *testing.T) {
+    tests := []struct {
+        name         string
+        acronymKey   string
+        acronymValue string
+        expected     string
+    }{
+        {
+            name:         "API Custom Acronym",
+            acronymKey:   "API",
+            acronymValue: "api",
+            expected:     "api",
+        },
+        {
+            name:         "ABCDACME Custom Acroynm",
+            acronymKey:   "ABCDACME",
+            acronymValue: "AbcdAcme",
+            expected:     "abcdAcme",
+        },
+        {
+            name:         "PostgreSQL Custom Acronym",
+            acronymKey:   "PostgreSQL",
+            acronymValue: "PostgreSQL",
+            expected:     "postgreSQL",
+        },
+    }
+    for _, test := range tests {
+        t.Run(
+            test.name, func(t *testing.T) {
+                ConfigureAcronym(test.acronymKey, test.acronymValue)
+                if result := S(test.acronymKey).ToLowerCamel(); result != test.expected {
+                    t.Errorf("expected custom acronym result %s, got %s", test.expected, result)
+                }
+            },
+        )
+    }
+}
+
+func BenchmarkToLowerCamel(b *testing.B) {
+    benchmarkCamelTest(b, toLowerCamel)
+}
+
+func benchmarkCamelTest(b *testing.B, fn func(testing.TB)) {
+    for n := 0; n < b.N; n++ {
+        fn(b)
+    }
+}
