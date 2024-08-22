ae48b5179c5cea61b2eb8b6e0fe5d942e85630a8
root <8974108+qwenode@users.noreply.github.com>
2024-08-22 19:40:23 +0800

    11

--- funcs.go
+++ funcs.go
@@ -1,5 +1,7 @@
 package rr
 
+import "time"
+
 func Catch(callable func() error) error {
     return callable()
 }
@@ -10,6 +12,17 @@ func Retry(count int, callable func() error) error {
         if err == nil {
             return nil
         }
+    }
+    return err
+}
+func RetryInterval(count int, sleepFor time.Duration, callable func() error) error {
+    var err error
+    for i := 0; i < count; i++ {
+        err = callable()
+        if err == nil {
+            return nil
+        }
+        time.Sleep(sleepFor)
     }
     return err
 }
