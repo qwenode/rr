806ef1edaf56ba59b561ae0d63af320216611487
root <8974108+qwenode@users.noreply.github.com>
2024-08-27 17:21:10 +0800

    11

--- funcs.go
+++ funcs.go
@@ -2,8 +2,18 @@ package rr
 
 import "time"
 
-func Catch(callable func() error) error {
-    return callable()
+func Catch(callable func() error, callables ...func() error) error {
+    err := callable()
+    if err != nil {
+        return err
+    }
+    for _, f := range callables {
+        err = f()
+        if err != nil {
+            return err
+        }
+    }
+    return nil
 }
 func Retry(count int, callable func() error) error {
     var err error
